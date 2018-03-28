d2bf402ac1f7a54b6483eead7f56a817b7116287
GUI Frame <byronguerra@byrons-air.mobile.asu.edu>
2018-03-28 08:51:10 -0700

    Added AirGarage API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import aglogo from './airgarageLogo.png';
 import './App.css';
 import Form from './Form';
 import LogIn from './LogIn';
 import Cards from './Cards';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div className = 'App'>
-        <h1 class="App-title">AirGarage</h1>
-        <Cards></Cards>
-
-    </div>
-    );
+  state = {
+      spots: []
   }
+  componentDidMount() {
+      axios.get(`https://airgara.ge/api/spots/`)
+        .then(res => {
+          const spots = res.data;
+          this.setState({ spots });
+        })
+  }
+
+render() {
+  return (
+    <div className="App">
+      <h1 class="App-title">AirGarage</h1>
+
+      <h2 className = "positionClasses">{ this.state.spots.map(spot => <Cards
+
+          name={spot.name}
+          streetAddress={spot.address.address_line1}
+          city={spot.address.city}
+          state={spot.address.state}
+          zip={spot.address.zipcode}
+          quantity={spot.quantity}
+          price={spot.price}
+          availability={spot.available_24_7}
+
+          />)}</h2>
+    </div>
+  );
+}
 }
 
 export default App;
--- src/Cards.js
+++ src/Cards.js
@@ -1,145 +1,64 @@
 import React, { Component } from 'react';
 import './Cards.css';
 import parkspot from './parkSpot.png';
+import aglogo from './airgarageLogo.png';
+import PropTypes from 'prop-types';
 
 class Cards extends React.Component {
-  constructor(props) {
-		super(props);
-		this.state = {
-		spotName: '',
-		notes: '',
-		restrictions: '',
-		pricePerMonth: '',
-		orgName: '',
-		allowAutoBook: '',
-		quantity:'',
-    spotPhoto: require('./parkSpot.png')
-	}
-}
 
+  constructor(props) {
+          super(props);
+          this.state = {
+              name: props.name,
+              streetAddress: props.address,
+              city: props.city,
+              state: props.state,
+              zip: props.zip,
+              price: props.price,
+              availability: props.availability,
+              quantity: props.quantity
+          };
+        }
 
   render() {
-  	return (
-    <div className = 'positionClasses'>
-      <div className = 'cardView'>
-        <img src={this.state.spotPhoto} className = "AG-logo" alt="aglogo"/>
-      <div>
-        <th>Location: </th>
-        <td>{this.state.spotName = 'Byrons spot'}</td>
-      </div>
+    return (
+      <div className='positionClasses'>
+        <div className='cardView'>
+          <img src={parkspot} className = "AG-logo" alt="aglogo"/>
 
-      <div>
-        <th>Notes: </th>
-        <td>{this.state.notes = 'Small cars only'}</td>
-      </div>
+          <div>
+            <th>Name: </th>
+          <td>{this.props.name}</td>
+          </div>
 
-      <div>
-        <th>Restrictions: </th>
-        <td>{this.state.restrictions = 'Weekdays only'}</td>
-      </div>
+          <div>
+            <th>Address: </th>
+          <td>{this.props.streetAddress}</td>
+          </div>
 
-      <div>
-        <th>Price Per Month: </th>
-        <td>{this.state.pricePerMonth = '$50'}</td>
-      </div>
+          <div>
+            <th>State: </th>
+          <td>{this.props.city}, {this.props.state}, {this.props.zip}</td>
+          </div>
 
-      <div>
-        <th>Organization Name: </th>
-        <td>{this.state.orgName = 'Air Garage'}</td>
-      </div>
+          <div>
+            <th>Spots Available: </th>
+          <td>{this.props.quantity}</td>
+          </div>
 
-      <div>
-        <th>Allow Auto Book?: </th>
-        <td>{this.state.allowAutoBook = 'Yes'}</td>
-      </div>
+          <div>
+            <th>Price per Month: </th>
+          <td>${this.props.price}</td>
+          </div>
 
-      <div>
-        <th>Quantity: </th>
-        <td>{this.state.quantity = '1'}</td>
-      </div>
-      </div>
-
-      <div className = 'cardView'>
-        <img src={this.state.spotPhoto} className = "AG-logo" alt="aglogo"/>
-
-        <div>
-          <th>Location: </th>
-          <td>{this.state.spotName = 'Loraines spot'}</td>
-        </div>
-
-        <div>
-          <th>Notes: </th>
-          <td>{this.state.notes = 'Walking distance to school'}</td>
-        </div>
-
-        <div>
-          <th>Restrictions: </th>
-          <td>{this.state.restrictions = 'No Restrictions'}</td>
-        </div>
-
-        <div>
-          <th>Price Per Month: </th>
-          <td>{this.state.pricePerMonth = '$30'}</td>
-        </div>
-
-        <div>
-          <th>Organization Name: </th>
-          <td>{this.state.orgName = 'Air Garage'}</td>
-        </div>
-
-        <div>
-          <th>Allow Auto Book?: </th>
-          <td>{this.state.allowAutoBook = 'Yes'}</td>
-        </div>
-
-        <div>
-          <th>Quantity: </th>
-          <td>{this.state.quantity = '2'}</td>
+          <div>
+            <th>Availability: </th>
+          <td>{this.props.availability ? "Available 24/7" : "Not Available 24/7"}</td>
+          </div>
         </div>
       </div>
-
-      <div className = 'cardView'>
-        <img src={this.state.spotPhoto} className = "AG-logo" alt="aglogo"/>
-
-        <div>
-          <th>Location: </th>
-          <td>{this.state.spotName = 'Mikes spot'}</td>
-        </div>
-
-        <div>
-          <th>Notes: </th>
-          <td>{this.state.notes = 'Safe Neighborhood'}</td>
-        </div>
-
-        <div>
-          <th>Restrictions: </th>
-          <td>{this.state.restrictions = 'Week days only'}</td>
-        </div>
-
-        <div>
-          <th>Price Per Month: </th>
-          <td>{this.state.pricePerMonth = '$45'}</td>
-        </div>
-
-        <div>
-          <th>Organization Name: </th>
-          <td>{this.state.orgName = 'Air Garage'}</td>
-        </div>
-
-        <div>
-          <th>Allow Auto Book?: </th>
-          <td>{this.state.allowAutoBook = 'Yes'}</td>
-        </div>
-
-        <div>
-          <th>Quantity: </th>
-          <td>{this.state.quantity = '1'}</td>
-        </div>
-      </div>
-    </div>
-
-          );
-      }
+    );
   }
+}
 
 export default Cards;
